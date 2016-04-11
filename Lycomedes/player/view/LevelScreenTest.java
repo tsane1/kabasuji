efa84fb6347a26e47aa154958f6d4706ef949a41
Tanuj Sane <tsane@wpi.edu>
2016-04-11 14:21:47 -0400

    I guess something changed? Idk really

--- Lycomedes/player/view/LevelScreenTest.java
+++ Lycomedes/player/view/LevelScreenTest.java
@@ -11,9 +11,6 @@ public class LevelScreenTest extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
