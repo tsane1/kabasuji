2a02d9bd875fe27edc13301c0871e34dca4caed1
dmmcmaster <dmmcmaster@wpi.edu>
2016-04-12 14:30:15 -0400

    added undo and redo controllers

--- Lycomedes/levelbuilder/model/LevelBuilderModel.java
+++ Lycomedes/levelbuilder/model/LevelBuilderModel.java
@@ -31,4 +31,33 @@ public class LevelBuilderModel {
 		undoStack.add(m);
 		redoStack.clear();
 	}
+	
+	/*
+	 * get/set undo moves
+	 */
+	public Move getLastMove(){
+		if(undoStack.isEmpty()) {
+			return null;
+		}
+		return undoStack.pop();
+	}
+	
+	public void addMoveToUndo(Move m){
+		undoStack.push(m);
+	}
+	
+	/*
+	 * get/set redo moves
+	 */
+	public void addRedoableMove(Move m){
+		redoStack.push(m);
+	}
+	
+	public Move getredoMove(){
+		if(redoStack.isEmpty()){
+			return null;
+		}
+		return redoStack.pop();
+	}
+	
 }
