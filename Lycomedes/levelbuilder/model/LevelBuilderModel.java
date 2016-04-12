090ebf8ab81265fae3c3dda9cde65d3a3c790af0
dmmcmaster <dmmcmaster@wpi.edu>
2016-04-12 12:47:38 -0400

    Added some stuff to level builder

--- Lycomedes/levelbuilder/model/LevelBuilderModel.java
+++ Lycomedes/levelbuilder/model/LevelBuilderModel.java
@@ -1,9 +1,34 @@
 package model;
 
+import java.util.Stack;
+import controller.Move;
+
 public class LevelBuilderModel {
 
-	public LevelBuilderModel() {
-		// TODO Auto-generated constructor stub
+	Level currentLevel;
+	Stack<Move> undoStack = new Stack<Move>();
+	Stack<Move> redoStack = new Stack<Move>();
+	
+	Piece selectedPiece;
+	
+	public LevelBuilderModel(Level L) {
+		this.currentLevel = L;
+	}
+	
+	public LevelBuilderModel(){
+		this(new Level());
+	}
+	
+	public Level getLevel(){
+		return currentLevel;
+	}
+	
+	public void setLevel(Level L){
+		this.currentLevel = L;
 	}
 
+	public void trackMoves(Move m){
+		undoStack.add(m);
+		redoStack.clear();
+	}
 }
