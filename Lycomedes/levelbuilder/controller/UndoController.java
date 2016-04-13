05e8f0d8bdd4d696e64bb5d96918289db7859dae
ijhuman <jo.gert@yandex.com>
2016-04-12 22:36:23 -0400

    ella ella eh eh

--- Lycomedes/levelbuilder/controller/UndoController.java
+++ Lycomedes/levelbuilder/controller/UndoController.java
@@ -35,8 +35,29 @@ public class UndoController implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 	// THIS IS A REQUIREMENT OF THE ACTIONLISTENER CLASS THE "doUndo" should prolly go here
 	}
 
+//What hienimon has under his WordMap undo controller	
+//	Model model;
+//	ApplicationPanel canvas;
+//	
+//	public UndoController(Model m, ApplicationPanel canvas) {
+//		this.model = m;
+//		this.canvas = canvas;
+//	}
+//	
+//	public boolean process() {
+//		Move m = model.removeLastMove();
+//		if (m == null) { return false; }
+//		
+//		if (m.undo()) {
+//			model.recordRedoableMove(m);
+//		}
+//		
+//		// force board to redraw
+//		canvas.redraw();
+//		canvas.repaint();
+//		return true;
+//	}
 }
