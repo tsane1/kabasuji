68a008a049a9d26ead5d68454fc680b4a7565039
Chase1325 <clstlaurent@wpi.edu>
2016-04-17 15:22:04 -0400

    Progress and ProgressView, Achievement and AchievementView classes

---
+++ Lycomedes/player/model/Achievement.java
@@ -0,0 +1,22 @@
+package model;
+
+import javax.swing.JProgressBar;
+import javax.swing.JWindow;
+
+import java.awt.Container;
+import java.awt.HeadlessException;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+
+public class Achievement {
+	Progress progress;
+
+	public Achievement(Progress progress){
+		super();
+		
+		this.progress = progress;
+	}
+}
