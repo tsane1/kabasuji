d7d2bbe10b3b819492f42892b81ca283f0a6231e
Chase1325 <clstlaurent@wpi.edu>
2016-04-18 12:22:50 -0400

    Updates to achievement/progress

---
+++ Lycomedes/player/model/Progress.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Progress {
+	Board board;
+	Tile boardArray[][];
+
+	public Progress(Board board, Tile[][] boardArray) {
+		this.board = board;
+		this.boardArray = boardArray;
+	}
+
+	public int updateProgressPuzzle() {
+		int value = 0;
+
+		// TODO: Puzzle -> Progress = (numberTilesCovered/numberTotalTiles)*100
+
+		return value;
+	}
+
+	public int updateProgressLightning() {
+		int value = 0;
+
+		// TODO: Lightning -> Progress = numberTilesCovered/numberTotalTile)*100
+
+		return value;
+	}
+
+	public int updateProgressRelease() {
+		int value = 0;
+
+		// TODO: Release -> Progress =
+		// numberReleaseTilesCovered/numberTotalReleaseTiles)*100
+
+		return value;
+	}
+}
