4e4ac4067c4a59fd06964579a27b6915f9be49db
ijhuman <jo.gert@yandex.com>
2016-04-18 11:46:20 -0400

    added rotation & flip controllers

---
+++ Lycomedes/levelbuilder/controller/Rot90RightController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.LevelBuilderModel;
+import view.LevelEdit;
+
+public class Rot90RightController implements ActionListener {
+
+	LevelBuilderModel model;
+	LevelEdit screen;
+	
+	public Rot90RightController(LevelEdit bs, LevelBuilderModel lbm) {
+		this.screen = bs;
+		this.model = lbm;
+	}
+	
+	public boolean doRot90Right(){
+		Move m = model.getLastMove();
+		
+		if(m == null){
+			return false;
+		}
+		
+		if(m.execute()) {
+			model.addMoveToUndo(m);
+		}
+		
+		//screen.redraw();
+		//screen.repaint();
+		
+		return true;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	// THIS IS A REQUIREMENT OF THE ACTIONLISTENER CLASS THE "doUndo" should prolly go here
+		try{
+			doRot90Right();
+		}
+		catch(Exception ex){
+			System.err.println("EXCEPTION CAUGHT : Rot90RightController");
+			ex.printStackTrace();
+		}
+	}
+}
+
+
