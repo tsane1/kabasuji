daea8095d4b3d54d63d9633a4375a663eb69ff86
Chase1325 <clstlaurent@wpi.edu>
2016-04-21 00:17:35 -0400

    Updated Achievement and Progress. Got MoveCounter and Timer Logic

--- Lycomedes/Kabasuji/kabasuji/entities/Timer.java
+++ Lycomedes/Kabasuji/kabasuji/entities/Timer.java
@@ -1,14 +1,38 @@
 package kabasuji.entities;
 
+import kabasuji.supers.Level;
+
 /** 
  * 
  * @author Chase St. Laurent
  *
  */
 public class Timer {
+	Level level;
+	
 
-	public void timer(){
-		
+	public Timer(){
+		super();
 	}
 	
+	public int timerCount(){
+		int count = 0;
+		if(!isLightning(level)){
+			count = 0; //OR infinity?
+		}
+		while(isLightning(level)){
+			//count = setBuilderTime();
+			count--;
+		}
+		return count;
+	}
+
+	public boolean isLightning(Level level){
+		boolean isLightning = false;
+		//TODO: Checks if game mode is lightning
+		if(level.getLevelName() == "Lightning"){
+			isLightning = true;
+		}
+		return isLightning;
+	}
 }
