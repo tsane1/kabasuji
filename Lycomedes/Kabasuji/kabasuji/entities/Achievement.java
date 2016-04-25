1e5200421b6837c7f154159b8fc5775bcdc8a57d
Chase1325 <clstlaurent@wpi.edu>
2016-04-25 14:19:50 -0400

    update achievement in level

--- Lycomedes/Kabasuji/kabasuji/entities/Achievement.java
+++ Lycomedes/Kabasuji/kabasuji/entities/Achievement.java
@@ -11,48 +11,50 @@ public class Achievement {
 
 	public Achievement(Progress progress) {
 		super();
-		this.progress = progress;
+		this.progress = progress; 
 	}
-
-	public boolean earnedAchievement() {
-		boolean isEarned = false;
-
-		if (isEarnedOneStar(progress) || isEarnedTwoStar(progress) || isEarnedThreeStar(progress)) {
-			isEarned = true;
-		}
-		return isEarned;
-	}
-
-	public boolean isEarnedOneStar(Progress progress) {
-		boolean isEarned = false;
-
-		if ((progress.updateProgressPuzzle() >= 50 && progress.updateProgressPuzzle() < 75)
-				|| (progress.updateProgressLightning() >= 50 && progress.updateProgressLightning() < 75)
-				|| (progress.updateProgressRelease() >= 32 && progress.updateProgressRelease() < 66)) {
-			isEarned = true;
-		}
-		return isEarned;
-	}
-
-	public boolean isEarnedTwoStar(Progress progress) {
-		boolean isEarned = false;
-
-		if ((progress.updateProgressPuzzle() >= 75 && progress.updateProgressPuzzle() < 100)
-				|| (progress.updateProgressLightning() >= 75 && progress.updateProgressLightning() < 100)
-				|| progress.updateProgressRelease() >= 66 && progress.updateProgressRelease() < 100) {
-			isEarned = true;
-		}
-		return isEarned;
-	}
-
-	public boolean isEarnedThreeStar(Progress progress) {
-		boolean isEarned = false;
-
-		if ((progress.updateProgressPuzzle() == 100) || progress.updateProgressLightning() == 100
-				|| progress.updateProgressRelease() == 100) {
-			isEarned = true;
-		}
-		return isEarned;
-	}
+	
+	///////////////DEAD CODE I THINK//////////////////////////////
+//
+//	public boolean earnedAchievement() {
+//		boolean isEarned = false;
+//
+//		if (isEarnedOneStar(progress) || isEarnedTwoStar(progress) || isEarnedThreeStar(progress)) {
+//			isEarned = true;
+//		}
+//		return isEarned;
+//	}
+//
+//	public boolean isEarnedOneStar(Progress progress) {
+//		boolean isEarned = false;
+//
+//		if ((progress.updateProgressPuzzle() >= 50 && progress.updateProgressPuzzle() < 75)
+//				|| (progress.updateProgressLightning() >= 50 && progress.updateProgressLightning() < 75)
+//				|| (progress.updateProgressRelease() >= 32 && progress.updateProgressRelease() < 66)) {
+//			isEarned = true;
+//		}
+//		return isEarned;
+//	}
+//
+//	public boolean isEarnedTwoStar(Progress progress) {
+//		boolean isEarned = false;
+//
+//		if ((progress.updateProgressPuzzle() >= 75 && progress.updateProgressPuzzle() < 100)
+//				|| (progress.updateProgressLightning() >= 75 && progress.updateProgressLightning() < 100)
+//				|| progress.updateProgressRelease() >= 66 && progress.updateProgressRelease() < 100) {
+//			isEarned = true;
+//		}
+//		return isEarned;
+//	}
+//
+//	public boolean isEarnedThreeStar(Progress progress) {
+//		boolean isEarned = false;
+//
+//		if ((progress.updateProgressPuzzle() == 100) || progress.updateProgressLightning() == 100
+//				|| progress.updateProgressRelease() == 100) {
+//			isEarned = true;
+//		}
+//		return isEarned;
+//	}
 
 }
