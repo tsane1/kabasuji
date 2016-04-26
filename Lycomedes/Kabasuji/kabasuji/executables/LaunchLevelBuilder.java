f96f59f6e8c69e9538445ce409026ab28c81c86b
ijhuman <jo.gert@yandex.com>
2016-04-26 00:06:27 -0400

    Brought back the builder of level launcher

---
+++ Lycomedes/Kabasuji/kabasuji/executables/LaunchLevelBuilder.java
@@ -0,0 +1,43 @@
+package kabasuji.executables;
+
+import kabasuji.supers.Application;
+
+import java.awt.EventQueue;
+
+import kabasuji.supers.Application;
+import kabasuji.supers.SuperModel;
+import kabasuji.views.BuilderSplashScreen;
+import kabasuji.views.LevelEditSelectView;
+import kabasuji.views.LevelPlayView;
+
+public class LaunchLevelBuilder {
+
+	/**
+	 * Launch the Builder application.
+	 * @throws InterruptedException
+	 * @author Ian +the peeps
+	 */
+	public static void main(String[] args) throws InterruptedException {
+
+		SuperModel m = new SuperModel();
+		final BuilderSplashScreen sc = new BuilderSplashScreen();
+
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				try {
+					sc.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});  
+
+		t.start();
+		Thread.sleep(3200);
+		sc.setVisible(false);
+		sc.dispose();
+		Application frame = new Application(new LevelEditSelectView(m));
+		frame.setVisible(true);
+	}
+}
+
