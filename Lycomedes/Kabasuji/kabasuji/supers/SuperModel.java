6afea0d1ebbac814c19e7dab13d8eff693b81b1e
Tanuj Sane <tsane@wpi.edu>
2016-04-26 10:50:57 -0400

    Levels regenerated

--- Lycomedes/Kabasuji/kabasuji/supers/SuperModel.java
+++ Lycomedes/Kabasuji/kabasuji/supers/SuperModel.java
@@ -215,6 +215,6 @@ public class SuperModel {
 	 */
 	public static void main(String[] args) {
 		SuperModel sm = new SuperModel();
-		sm.generateDefaultLevels();
+		//sm.generateDefaultLevels();
 	}
 }
