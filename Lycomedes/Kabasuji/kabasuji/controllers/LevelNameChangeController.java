d98599a95a4b306eb92fccd9826bfada70be4350
Tanuj Sane <tsane@wpi.edu>
2016-04-26 23:12:33 -0400

    Level renaming controller added

---
+++ Lycomedes/Kabasuji/kabasuji/controllers/LevelNameChangeController.java
@@ -0,0 +1,40 @@
+package kabasuji.controllers;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.JTextField;
+
+import kabasuji.supers.Application;
+import kabasuji.supers.SuperModel;
+
+public class LevelNameChangeController implements FocusListener {
+	
+	private SuperModel model;
+	private Application app;
+
+	public LevelNameChangeController(Application app, SuperModel model) {
+		this.app = app;
+		this.model = model;
+	}
+	
+	@Override
+	public void focusGained(FocusEvent e) {
+		System.out.println("focus on" + e.getComponent().toString());
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		model.deleteLevel(model.getUserLevelDir(), model.getActiveLevel().getLevelName()+".lev");
+		String newTitle;
+		if(app.getCurrScreen().getTitle().contains("New")) {
+			newTitle = "New " + model.getActiveLevel().getLevelType() + " Level: " + ((JTextField)e.getComponent()).getText();
+		}
+		else {
+			newTitle = ((JTextField)e.getComponent()).getText() + ": " + model.getActiveLevel().getLevelType() + " [EDIT]";
+		}
+		app.getCurrScreen().setTitle(newTitle);
+		model.getActiveLevel().setLevelName(((JTextField)e.getComponent()).getText());
+	}
+
+}
