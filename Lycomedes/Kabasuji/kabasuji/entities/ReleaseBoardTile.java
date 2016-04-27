4dcaf5ac7deee4380316a6856b60bf1c3fb10126
ijhuman <jo.gert@yandex.com>
2016-04-26 21:58:33 -0400

    errz gone again class added back

---
+++ Lycomedes/Kabasuji/kabasuji/entities/ReleaseBoardTile.java
@@ -0,0 +1,106 @@
+package kabasuji.entities;
+
+/**
+ * Tile Subclass which will make up the Release Level Boards.
+ * 
+ * @author Derek McMaster
+ * @author Michael
+ * @author Ian Jacoway
+ */
+public class ReleaseBoardTile extends Tile{
+
+	/**
+	 * serial Id for releaseboardtile.
+	 */
+	private static final long serialVersionUID = -2218330060081200831L;
+	/** row and column attributes for a tile object. */
+	int row, col;
+	/** attribute for whether or not a tile is covered. */
+	private boolean covered;
+	
+
+	/** value of the tile, a number that will be translated into a color later */
+	int value, numColor;
+	
+	/**
+	 * Constructor for the releaseboardtile object.
+	 * @param int row
+	 * @param int col
+	 */
+	public ReleaseBoardTile(int row, int column) {
+		super(row, column);
+		
+		
+		this.row = row;
+		this.col = column;
+		value = 0;
+		covered = false;
+		
+	}
+
+	/**
+	 * Method to change a tile to covered state.
+	 * @return void
+	 */
+	public void cover() {
+		covered = true;
+	}
+	
+	/**
+	 * Method to change a tile to uncovered state.
+	 * @return void
+	 */
+	public void uncover(){
+		covered = false;
+	}
+	
+	/**
+	 * Method to determine a tile's covered state.
+	 * @return boolean
+	 */
+	public boolean isCovered() {
+		return covered;
+	}
+
+	/**
+	 * Increments release number supports up to six then resets to zero.
+	 */
+	public void updateReleaseNum(){
+		if(value<=6){
+			value++;
+		}else{
+			value = 0;
+		}
+		
+	}
+
+	/**
+	 * Increments release number's color supports up to six,
+	 * each number corresponds to a color, default color is black at value zero.
+	 */
+	public void updateReleaseColor(){
+		if(numColor<=6){
+			numColor++;
+		}else{
+			numColor= 0;
+		}
+		
+	}
+
+	/**	
+	 * Gets the value or number on the tile.
+	 * @return int
+	 */
+	public int getValue(){
+		return value;
+	}
+
+	/**	
+	 * Gets the value or number on the tile.
+	 * @return int
+	 */
+	public int getNumColor(){
+		return numColor;
+	}
+
+}
