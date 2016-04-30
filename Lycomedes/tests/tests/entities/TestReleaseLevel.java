3308861e40a68bd9d6f7058bb457b5e602de6fba
dmmcmaster <dmmcmaster@wpi.edu>
2016-04-30 18:16:47 -0400

    testing

---
+++ Lycomedes/tests/tests/entities/TestReleaseLevel.java
@@ -0,0 +1,39 @@
+package tests.entities;
+
+import junit.framework.TestCase;
+import kabasuji.entities.PuzzleLevel;
+import kabasuji.entities.ReleaseLevel;
+
+public class TestReleaseLevel extends TestCase {
+
+	ReleaseLevel testLevel;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		testLevel = new ReleaseLevel("test");
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testReleaseChars(){
+		assertEquals("test", testLevel.getLevelName());
+		
+		assertEquals("Release", testLevel.getLevelType());
+		
+		assertTrue(testLevel.getNumsCollectedByColor("red").isEmpty());
+		assertTrue(testLevel.getNumsCollectedByColor("yellow").isEmpty());
+		assertTrue(testLevel.getNumsCollectedByColor("green").isEmpty());
+		
+		testLevel.collect("red", 1);
+		testLevel.collect("yellow", 1);
+		testLevel.collect("green", 1);
+		
+		assertFalse(testLevel.getNumsCollectedByColor("red").isEmpty());
+		assertFalse(testLevel.getNumsCollectedByColor("yellow").isEmpty());
+		assertFalse(testLevel.getNumsCollectedByColor("green").isEmpty());
+	}
+
+}
