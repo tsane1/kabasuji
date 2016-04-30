42bfb80a2fde80fc120ca3718f0107ecde9b5b5d
dmmcmaster <dmmcmaster@wpi.edu>
2016-04-30 12:29:41 -0400

    fixed small merge issue

--- Lycomedes/Kabasuji/kabasuji/entities/PieceDrawer.java
+++ Lycomedes/Kabasuji/kabasuji/entities/PieceDrawer.java
@@ -24,11 +24,25 @@ public class PieceDrawer {
 	 * @param Piece p
 	 */
 	public void drawPiece(Graphics g, Piece p, int x, int y, Color c) {
-		for(PieceTile pt : p.getTileLocations()){
+		int rowAdjust = p.tiles[0].row;
+		int colAdjust = p.tiles[0].col;
+		
+		for(int i = 0; i<p.numTilesInPiece(); i++){
+			
+			int rowCord = y + ((p.tiles[i].row - rowAdjust)*32);
+			int colCord = x + ((p.tiles[i].col - colAdjust)*32);
 			g.setColor(c);
-			g.fillRect(x+(Tile.width*pt.getColumn()), y+(Tile.height*pt.getRow()), Tile.width, Tile.height);
+			g.fillRect(colCord, rowCord, Tile.width, Tile.height);
 			g.setColor(Color.black);
-			g.drawRect(x+(Tile.width*pt.getColumn()), y+(Tile.height*pt.getRow()), Tile.width, Tile.height);
+			g.drawRect(colCord, rowCord, Tile.width, Tile.height);
+		}
+	}
+	public void drawPalettePiece(Graphics g, Piece p, int x, int y, Color c){
+		for(PieceTile pt: p.getTileLocations()){
+			g.setColor(c);
+			g.fillRect(x + (pt.getColumn() * Tile.width), y + (pt.getRow() * Tile.height), Tile.width, Tile.height);
+			g.setColor(Color.BLACK);
+			g.drawRect(x + (pt.getColumn() * Tile.width), y + (pt.getRow() * Tile.height), Tile.width, Tile.height);
 		}
 	}
 	
