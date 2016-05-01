1922e2368426860e450410ae657d6b40da1092d6
JabbaDuhHutt <michael.pickett72@gmail.com>
2016-05-01 15:49:39 -0400

    Shifting to center for numbers

--- Lycomedes/Kabasuji/kabasuji/views/BoardView.java
+++ Lycomedes/Kabasuji/kabasuji/views/BoardView.java
@@ -183,16 +183,16 @@ public class BoardView extends JPanel {
 						offScreenGraphics.setFont(new Font("Kristen ITC", Font.BOLD, 16));
 						if(((ReleaseBoardTile) boardArray[i][j]).getValue() > 6 && ((ReleaseBoardTile) boardArray[i][j]).getValue() <= 12){
 							offScreenGraphics.setColor(Color.GREEN.darker());
-							offScreenGraphics.drawString(Integer.toString(((ReleaseBoardTile) boardArray[i][j]).getValue()-6), (i*Tile.width + (Tile.width / 2)), (j*Tile.height + (Tile.height / 2)));
+							offScreenGraphics.drawString(Integer.toString(((ReleaseBoardTile) boardArray[i][j]).getValue()-6), (i*Tile.width + 13), (j*Tile.height + 20));
 
 						}else if(((ReleaseBoardTile) boardArray[i][j]).getValue() > 12){
 							offScreenGraphics.setColor(Color.RED.darker());
-							offScreenGraphics.drawString(Integer.toString(((ReleaseBoardTile) boardArray[i][j]).getValue()-12), (i*Tile.width + (Tile.width / 2)), (j*Tile.height + (Tile.height / 2)));
+							offScreenGraphics.drawString(Integer.toString(((ReleaseBoardTile) boardArray[i][j]).getValue()-12), (i*Tile.width + 13), (j*Tile.height + 20));
 
 						}else{
 
 							offScreenGraphics.setColor(Color.YELLOW);
-							offScreenGraphics.drawString(Integer.toString(((ReleaseBoardTile) boardArray[i][j]).getValue()), (i*Tile.width + (Tile.width / 2)), (j*Tile.height + (Tile.height / 2)));
+							offScreenGraphics.drawString(Integer.toString(((ReleaseBoardTile) boardArray[i][j]).getValue()), (i*Tile.width + 13), (j*Tile.height + 20));
 
 						}
 
