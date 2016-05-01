3264a860cf5d1f4fbccd88a089247d1cf1b33a87
Tanuj Sane <tsane@wpi.edu>
2016-05-01 15:31:54 -0400

    blah

--- Lycomedes/Kabasuji/kabasuji/views/LevelEndView.java
+++ Lycomedes/Kabasuji/kabasuji/views/LevelEndView.java
@@ -1,5 +1,9 @@
 package kabasuji.views;
 
+import java.awt.Font;
+import java.awt.SystemColor;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import kabasuji.supers.Application;
@@ -9,6 +13,10 @@ import kabasuji.supers.SuperModel;
 public class LevelEndView extends Screen {
 	private SuperModel model;
 	
+	private JButton btnLevelSelect = new JButton();
+	private JButton btnContinue = new JButton();
+	private JButton btnReplay = new JButton();
+	
 	public LevelEndView(SuperModel m) {
 		super("", m);
 		this.model = m;
@@ -16,7 +24,21 @@ public class LevelEndView extends Screen {
 
 	@Override
 	public void populate() {
+		btnLevelSelect.setBackground(SystemColor.text);
+		btnLevelSelect.setForeground(SystemColor.textHighlight);
+		btnLevelSelect.setFont(new Font("Kristen ITC", Font.BOLD, 16));
+		btnLevelSelect.setBounds(87, 409, 100, 50);
 		
+//		if(model.getActiveLevel().hasEnded && !model.getActiveLevel().hasWon) {
+//			setTitle(model.getActiveLevel().getLevelName() + " LOST");
+//		}
+//		else if(model.getActiveLevel().hasEnded && model.getActiveLevel().hasWon) {
+//			setTitle(model.getActiveLevel().getLevelName() + " WON");
+//			this.add(btnContinue);
+//		}
+		
+		this.add(btnLevelSelect);
+		this.add(btnReplay);
 	}
 
 	@Override
