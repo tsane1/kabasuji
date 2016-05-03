959239ac84b83a71ea478777d218247c95650823
dmmcmaster <dmmcmaster@wpi.edu>
2016-05-03 17:12:16 -0400

    place piece works

--- Lycomedes/tests/tests/entities/testPlacePiece.java
+++ Lycomedes/tests/tests/entities/testPlacePiece.java
@@ -34,15 +34,47 @@ public class testPlacePiece extends TestCase {
 	}
 	
 	public void testPlace(){
-		//grab piece in second indices
-		Piece testPiece = bullpen.getPieces().get(4);
-		bullpen.removePiece(testPiece);
+		Piece testPiece;
+		PieceTile[] arr;
+		/** set up a test piece */
+		arr = new PieceTile[6];
+		PieceTile pt1 = new PieceTile(0,0);
+		arr[0] = pt1;
+		PieceTile pt2 = new PieceTile(1,0);
+		arr[1] = pt2;
+		PieceTile pt3 = new PieceTile(2,0);
+		arr[2] = pt3;
+		PieceTile pt4 = new PieceTile(3,0);
+		arr[3] = pt4;
+		PieceTile pt5 = new PieceTile(4,0);
+		arr[4] = pt5;
+		PieceTile pt6 = new PieceTile(5,0);
+		arr[5] = pt6;
+		
+		testPiece = new Piece(1, arr);
+
 		if(board.place(2, 3, testPiece)){
 			assertEquals(true, board.getPlacedPieces().containsValue(testPiece));
 		}else{
 			System.err.println("place returned false");
 		}
-		Piece testPiece2 = bullpen.getPieces().get(2);
+
+		/** set up a test piece */
+		PieceTile[] arr2 = new PieceTile[6];
+		PieceTile pt7 = new PieceTile(5,0);
+		arr2[0] = pt7;
+		PieceTile pt8 = new PieceTile(4,0);
+		arr2[1] = pt8;
+		PieceTile pt9 = new PieceTile(3,0);
+		arr2[2] = pt9;
+		PieceTile pt10 = new PieceTile(2,0);
+		arr2[3] = pt10;
+		PieceTile pt11 = new PieceTile(1,0);
+		arr2[4] = pt11;
+		PieceTile pt12 = new PieceTile(0,0);
+		arr2[5] = pt12;
+		
+		Piece testPiece2 = new Piece(2, arr2);
 		bullpen.removePiece(testPiece2);
 		assertEquals(false, board.place(2, 3, testPiece2));
 		
