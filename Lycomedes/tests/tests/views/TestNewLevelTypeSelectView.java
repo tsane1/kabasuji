f8b876f4699452b4a150dd2f100e9312463f3776
dmmcmaster <dmmcmaster@wpi.edu>
2016-05-03 16:55:08 -0400

    tests

---
+++ Lycomedes/tests/tests/views/TestNewLevelTypeSelectView.java
@@ -0,0 +1,31 @@
+package tests.views;
+
+import junit.framework.TestCase;
+import kabasuji.supers.SuperModel;
+import kabasuji.views.NewLevelTypeSelectView;
+
+public class TestNewLevelTypeSelectView extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testView(){
+		
+		SuperModel sm = new SuperModel();
+		NewLevelTypeSelectView nv = new NewLevelTypeSelectView(sm);
+		
+		nv.refresh();
+		assertTrue(nv.getName().equals("NewLevelTypeSelectView"));
+		
+		assertNull(nv.getBoardView());
+		assertNull(nv.getBullpenView());
+		assertNull(nv.getLevel());
+		assertNull(nv.getPaletteView());
+	}
+
+}
